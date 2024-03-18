da44c7bdeabbde441dc3ebfec51a80959ef26055
bwire40 <emmanuelbwire80@gmail.com>
2024-03-18 10:17:13 +0300

    new commits

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,8 +76,15 @@
                         d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
                 </svg> --}}
                 <div class="flex flex-col justify-center items-center">
-                    <img class="hover:grow shadow-lg w-12 h-12 rounded-full" src="https://rb.gy/y795kq"
-                        width="100px" />
+                    @Auth
+                        <img class="hover:grow shadow-lg w-12 h-12 rounded-full"
+                            src="{{ asset('images/' . Auth::user()->image) }}" width="100px" />
+                    @else
+                        <img class="hover:grow shadow-lg w-12 h-12 rounded-full" src="https://rb.gy/y795kq"
+                            width="100px" />
+
+                    @endauth
+
 
 
                     <div class="font-bold text-xs text-indigo-800">{{ Auth::user()->name ?? false }}</div>
